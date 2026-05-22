6298053955360c29d0b23e1ffff7a82974dc56f6
khukrish-star <khukrish@gmail.com>
2026-05-22 19:52:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot working successfully ✅")
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
